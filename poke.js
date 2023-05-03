5cbb7f05810a3557ec773b55de4b6c134e211f41
shmeluizze <127356288+shmeluizze@users.noreply.github.com>
2023-05-03 14:07:59 +0100

    Update poke.js

--- poke.js
+++ poke.js
@@ -1,103 +1,15 @@
 
 
-document.addEventListener("DOMContentLoaded", () =>{
+const toggleBtn = document.querySelector(".lines-container");
+let line1 = document.querySelector(".line1")
+let line2 = document.querySelector(".line2")
+let line3 = document.querySelector(".line3")
 
-    let generateBtn = document.querySelector('#generate-pokemon');
-    generateBtn.addEventListener('click', renderEverything)
-
-    getDeleteBtn().addEventListener('click', deleteEverything);
-})
-
-function renderEverything(){
-    let allPokemonContainer = document.querySelector('#poke-container')
-    allPokemonContainer.innerText = "";
-    fetchKantoPokemon();
-
-    getDeleteBtn().style.display = 'block'
+function toggleMenu() {
+    const navbarToggle = document.getElementById("main-navbar");
+    navbarToggle.classList.toggle("menu-active");
+    line1.classList.toggle("active");
+    line2.classList.toggle("active");
+    line3.classList.toggle("active");
 }
-
-function getDeleteBtn(){
-    return document.querySelector('#delete-btn')
-}
-
-
-function fetchKantoPokemon() {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-        .then(response => response.json())
-        .then(function(allpokemon) {
-            allpokemon.results.sort(function(a, b) {
-                return a.id - b.id;
-            });
-            allpokemon.results.forEach(function(pokemon) {
-                fetchPokemonData(pokemon);
-            })
-        })
-}
-
-
-function fetchPokemonData(pokemon){
-    let url = pokemon.url // <--- this is saving the pokemon url to a variable to use in the fetch. 
-                                //Example: https://pokeapi.co/api/v2/pokemon/1/"
-    fetch(url)
-    .then(response => response.json())
-    .then(function(pokeData){
-        renderPokemon(pokeData)
-    })
-}
-
-
-function renderPokemon(pokeData){
-    let allPokemonContainer = document.getElementById('poke-container');
-    let pokeContainer = document.createElement("div") //div will be used to hold the data/details for indiviual pokemon.{}
-    pokeContainer.classList.add('ui', 'card');
-
-    createPokeImage(pokeData.id, pokeContainer);
-
-    let pokeName = document.createElement('h4') 
-    pokeName.innerText = pokeData.name
-
-    let pokeNumber = document.createElement('p')
-    pokeNumber.innerText = `#${pokeData.id}`
-   
-    let pokeTypes = document.createElement('ul') //ul list will hold the pokemon types
-  
-
-    createTypes(pokeData.types, pokeTypes) // helper function to go through the types array and create li tags for each one
-
-    pokeContainer.append(pokeName, pokeNumber, pokeTypes);   //appending all details to the pokeContainer div
-    allPokemonContainer.appendChild(pokeContainer);       //appending that pokeContainer div to the main div which will                                                             hold all the pokemon cards
-}
-
-function createTypes(types, ul){
-    types.forEach(function(type){
-        let typeLi = document.createElement('li');
-        typeLi.innerText = type['type']['name'];
-        ul.append(typeLi)
-    })
-}
-
-function createPokeImage(pokeID, containerDiv) {
-    let pokeImgContainer = document.createElement('div')
-    pokeImgContainer.classList.add('image')
-  
-    let pokeImage = document.createElement('img')
-    pokeImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeID}.png`
-  
-    pokeImgContainer.append(pokeImage);
-    containerDiv.append(pokeImgContainer);
-  }
-  
-
-function deleteEverything(event){
-    event.target.style = 'none';
-    let allPokemonContainer = document.querySelector('#poke-container')
-    allPokemonContainer.innerText = ""
-
-    let generateBtn = document.createElement('button')
-    generateBtn.innerText = "Generate Pokemon"
-    generateBtn.id = 'generate-pokemon'
-    generateBtn.classList.add('ui', 'secondary', 'button')
-    generateBtn.addEventListener('click', renderEverything);
-
-    allPokemonContainer.append(generateBtn)
-}
+toggleBtn.addEventListener("click", toggleMenu);
