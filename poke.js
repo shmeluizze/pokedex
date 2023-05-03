cc7cd57bbaaf4ff5ae51d0f3e3310dd669bf3e91
shmeluizze <127356288+shmeluizze@users.noreply.github.com>
2023-05-03 18:15:34 +0100

    Update poke.js

--- poke.js
+++ poke.js
@@ -13,3 +13,66 @@ function toggleMenu() {
     line3.classList.toggle("active");
 }
 toggleBtn.addEventListener("click", toggleMenu);
+
+
+// Define constants for the API endpoints
+const API_ENDPOINTS = {
+  'kanto': 'https://pokeapi.co/api/v2/pokemon?limit=151',
+  'johto': 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=151',
+  'hoenn': 'https://pokeapi.co/api/v2/pokemon?limit=135&offset=251'
+};
+
+// Define a function to fetch and display Pokemon cards
+async function fetchPokemon(region) {
+  try {
+    // Fetch the Pokemon data for the specified region
+    const response = await fetch(API_ENDPOINTS[region]);
+    const data = await response.json();
+    
+    // Map over the results and create a card for each Pokemon
+    const cards = data.results.map(async (result) => {
+      const pokemonResponse = await fetch(result.url);
+      const pokemonData = await pokemonResponse.json();
+      
+      // Extract the relevant data from the Pokemon object
+      const id = pokemonData.id;
+      const name = pokemonData.name;
+      const image = pokemonData.sprites.front_default;
+      const type = pokemonData.types[0].type.name;
+      
+      // Create the HTML for the card
+      return `
+        <div class="card">
+          <img src="${image}" alt="${name}">
+          <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <p class="card-text">Type: ${type}</p>
+            <p class="card-text">ID: ${id}</p>
+          </div>
+        </div>
+      `;
+    });
+    
+    // Wait for all the cards to be created before displaying them
+    const cardHTML = await Promise.all(cards);
+    
+    // Display the cards on the page
+    const regionContainer = document.querySelector('.region-container');
+    regionContainer.innerHTML = cardHTML.join('');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Add event listeners to the region buttons
+const kantoButton = document.querySelector('#kanto-button');
+kantoButton.addEventListener('click', () => fetchPokemon('kanto'));
+
+const johtoButton = document.querySelector('#johto-button');
+johtoButton.addEventListener('click', () => fetchPokemon('johto'));
+
+const hoennButton = document.querySelector('#hoenn-button');
+hoennButton.addEventListener('click', () => fetchPokemon('hoenn'));
+
+
+
