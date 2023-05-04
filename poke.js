dfb0425e3dfa089d98d0c5bcbf412579a1227c31
shmeluizze <127356288+shmeluizze@users.noreply.github.com>
2023-05-04 11:05:06 +0100

    Update poke.js

--- poke.js
+++ poke.js
@@ -15,63 +15,91 @@ function toggleMenu() {
 toggleBtn.addEventListener("click", toggleMenu);
 
 
-// Define constants for the API endpoints
 const API_ENDPOINTS = {
-  'kanto': 'https://pokeapi.co/api/v2/pokemon?limit=151',
-  'johto': 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=151',
-  'hoenn': 'https://pokeapi.co/api/v2/pokemon?limit=135&offset=251'
+    'kanto': 'https://pokeapi.co/api/v2/pokemon?limit=151',
+    'johto': 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=151',
+    'hoenn': 'https://pokeapi.co/api/v2/pokemon?limit=135&offset=251'
 };
- 
+
+// Fetch the Pokemon data for the specified region
 async function fetchPokemon(region) {
-  try {
-    // Fetch the Pokemon data for the specified region
-    const response = await fetch(API_ENDPOINTS[region]);
-    const data = await response.json();
-    
-
-    const cards = data.results.map(async (result) => {
-      const pokemonResponse = await fetch(result.url);
-      const pokemonData = await pokemonResponse.json();
-      
-
-      const id = pokemonData.id;
-      const name = pokemonData.name;
-      const image = pokemonData.sprites.front_default;
-      const type = pokemonData.types[0].type.name;
-      
-      // Create the HTML for the card
-      return `
-        <div class="card">
-          <img src="${image}" alt="${name}">
-          <div class="card-body">
-            <h5 class="card-title">${name}</h5>
-            <p class="card-text">Type: ${type}</p>
-            <p class="card-text">ID: ${id}</p>
-          </div>
-        </div>
-      `;
+    const response = await fetch(API_ENDPOINTS[region], {
+        headers: {
+            'content-type': 'application/json;charset=UTF-8'
+        }
     });
-    
-    
-    const cardHTML = await Promise.all(cards);
-    
-    
-    const regionContainer = document.querySelector('.region-container');
-    regionContainer.innerHTML = cardHTML.join('');
-  } catch (error) {
-    console.error(error);
-  }
+    const data = await response.json();
+
+    const pokemonList = await Promise.all(data.results.map(async(result) => {
+        const pokemonResponse = await fetch(result.url);
+        const pokemonData = await pokemonResponse.json();
+        const pokemon = {
+            id: pokemonData.id,
+            name: pokemonData.name,
+            image: pokemonData.sprites.front_default,
+            type: pokemonData.types[0].type.name
+        };
+        return pokemon;
+    }));
+
+    return pokemonList;
 }
 
-// Add event listeners to the region buttons
+
+function renderAll(pokemonList) {
+
+    const cardContainers = document.querySelectorAll('.ui.cards .card');
+    cardContainers.forEach(container => container.remove());
+
+    //new card container and add the cards to it
+    const pokeContainer = document.getElementById('poke-container');
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('card');
+    pokemonList.forEach(pokemon => {
+        const card = document.createElement('div');
+        card.innerHTML = `
+        <img src="${pokemon.image}" alt="${pokemon.name}">
+        <div class="card-body">
+          <h5 class="card-title">${pokemon.name}</h5>
+          <p class="card-text">Type: ${pokemon.type}</p>
+          <p class="card-text">ID: ${pokemon.id}</p>
+        </div>`;
+        cardContainer.appendChild(card);
+    });
+    pokeContainer.appendChild(cardContainer);
+    console.log(pokeContainer.offsetTop)
+    window.scrollTo({
+        top: pokeContainer.offsetTop - 200,
+        behavior: "smooth"
+    })
+
+}
+
 const kantoButton = document.querySelector('#kanto-button');
-kantoButton.addEventListener('click', () => fetchPokemon('kanto'));
+kantoButton.addEventListener('click', () => {
+
+    fetchPokemon('kanto').then(pokemonList => {
+        console.log(pokemonList);
+        renderAll(pokemonList);
+    }).catch(error => {
+        console.error(error);
+    });
+});
 
 const johtoButton = document.querySelector('#johto-button');
-johtoButton.addEventListener('click', () => fetchPokemon('johto'));
+johtoButton.addEventListener('click', () => {
+    fetchPokemon('johto').then(pokemonList => {
+        renderAll(pokemonList);
+    }).catch(error => {
+        console.error(error);
+    });
+});
 
 const hoennButton = document.querySelector('#hoenn-button');
-hoennButton.addEventListener('click', () => fetchPokemon('hoenn'));
-
-
-
+hoennButton.addEventListener('click', () => {
+    fetchPokemon('hoenn').then(pokemonList => {
+        renderAll(pokemonList);
+    }).catch(error => {
+        console.error(error);
+    });
+});
